b93e42cf9390b7d248aaa8eec012b90266b78aac
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-06 20:24:02 +0700

    ubah nav icon

--- app/Filament/Resources/Students/StudentResource.php
+++ app/Filament/Resources/Students/StudentResource.php
@@ -22,7 +22,7 @@ class StudentResource extends Resource
 {
     protected static ?string $model = Student::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::UserGroup;
 
     protected static ?string $recordTitleAttribute = 'Student';
 
