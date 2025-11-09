f29200489fee0f89f44abed5b26975c458711999
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-09 17:22:49 +0700

    install api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 
 Route::get('/students', [StudentController::class, 'index']);
+
+Route::get('/test', function() {
+    return response()->json(['message' => 'API connected']);
+});
