bb98f6d73bf5b5b6c59e082eac9070eae9c0076f
WahyuTomo14 <wahyuxd14@gmail.com>
2025-11-09 20:46:04 +0700

    buat route api lecture

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\LectureController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 
 Route::get('/students', [StudentController::class, 'index']);
+
+Route::get('/lectures', [LectureController::class, 'index']);
 
 Route::get('/test', function() {
     return response()->json(['message' => 'API connected']);
